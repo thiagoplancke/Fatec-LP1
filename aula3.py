dbe2ad6d16bc2c2a876f39ed55a489cc32644172
Thiago Plancke <thiagoplancke@gmail.com>
2025-09-12 18:53:42 -0300

    codigo

---
+++ aula3.py
@@ -0,0 +1,24 @@
+salario = 100
+idade = 20
+
+
+print(salario < 500 and idade > 18 ) 
+print(salario >= 100 and idade < 30)   
+print(salario == 100 and idade >= 20)
+print(salario != 50 and idade <= 25) 
+print(salario > 50 and idade == 20)  
+print(salario < 1000 or idade > 25)  
+print(salario >= 200 or idade < 18)   
+print(salario == 100 or idade >= 30)   
+print(salario != 100 and idade < 21)
+print(salario > 200 and idade > 15) 
+print(salario <= 100 and idade >= 20) 
+print(salario < 500 or idade == 20) 
+print(salario > 200 and not idade > 18) 
+print(salario == 100 and not idade != 20)   
+print(salario != 100 or idade > 30)   
+print(not (salario > 1000 and idade > 18))
+print(not (salario == 100 and idade < 18) )
+print( not (salario < 50 or idade > 40) )
+print(not (salario >= 500 or idade <= 18) ) 
+print(not (salario != 100 and idade == 20)) 
---
+++ aula4.py
@@ -0,0 +1,115 @@
+## Exercicios de fixação
+# 1) Contagem Progressiva
+
+# num = int(input("Digite um numero: "))
+# x = 0
+# while x < num:
+#     x += 1
+#     print(x)
+
+#2) senha correta
+
+# senha = "123mudar"
+
+# while True:
+#     digite = input("Digite a senha: ")
+#     if digite == senha:
+#        print("Acesso permitido!")
+#        break
+#     else:
+#         print("Senha errada!")
+
+# 3) soma de numeros
+
+# soma = 0
+# while True:
+#     num = int(input("Digite um numero: "))
+#     soma += num
+#     if num == 0:
+#         print(soma)
+#         break
+
+
+## Exercicio de fixação 2
+
+# 1) Contagem Progressiva
+
+# num = int(input("Digite um numero: "))
+
+# for i in range(1, num + 1):
+#     print(i)
+
+
+# 2) Tabuada 
+
+# num = int(input("Digite um numero: "))
+
+# for i in range(1, 10+1):
+#     print(f"{num} x {i} = {num*i}")
+
+# 3) Numero par de 1 a 20
+
+# for i in range(21):
+#     if i % 2 == 0:
+#         print(i)
+
+## Exercicio de fixação 3
+
+# 1) contagem de 1 a 10 usando for e while
+
+# for i in range(1,11):
+#     print(i)
+
+# x = 1
+# while True:
+#     print(x)
+#     x += 1
+#     if x > 10:
+#         break
+
+
+# 2) Multiplos de 5 a 50
+
+# for i in range(5,51,5):
+#     print(i)
+
+# x = 50
+# somador = 5
+
+# while True:
+#     print(somador)
+#     somador += 5
+#     if somador > x:
+#         break
+
+
+# 3) Soma de numero pares
+
+# num = int(input("Digite um numero: "))
+# soma = 0
+# for i in range(1, num + 1):
+#     if i % 2 == 0:
+#         soma += i
+# print(soma)
+
+# 4) Calculo fatoria usando for e while
+
+# num = int(input("Digite um numero: "))
+# x = 1
+# for i in range(1, num +1):
+#     x *= i 
+# print(x)
+
+# numero = int(input("Digite um número: "))
+# fatorial = 1
+# contador = 1
+
+# while contador <= numero:
+#     fatorial *= contador
+#     contador += 1
+
+# print(f"O fatorial de {numero} é {fatorial}")
+
+
+
+
