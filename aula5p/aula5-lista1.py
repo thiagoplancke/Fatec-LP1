fb1b1bcfdc308ca4875b944067c09b7fc0ce8cc6
Thiago Plancke <thiagoplancke@gmail.com>
2025-09-17 16:57:49 -0300

    atualização

--- aula5p/aula5-lista1.py
+++ aula5p/aula5-lista1.py
@@ -12,3 +12,25 @@
 # num2 = int(input("Digite o segundo numero: "))
 
 # print(f"A soma de {num} + {num2} == {num + num2}")
+
+
+#3. Solicite dois números e exiba a média.
+
+# num = int(input("Digite não um numero: "))
+# num1 = int(input("Digite outro numero: "))
+# print(f"A média é {(num+num1)/2}")
+
+
+# 4. Peça o raio de um círculo e calcule a área.
+
+# raio =  float(input("Digite o raio do circulo: "))
+# print(f"A area do circulo é {3.14 * (raio**2)}m²")
+
+
+# 5. Peça o lado de um quadrado e calcule a área e o perímetro
+
+# lado = float(input("Digite o lado de um quadrado: "))
+# area = lado*lado
+# perimetro = lado * 4
+
+# print(f"Area: {area}m² e Perimetro: {perimetro}m")
